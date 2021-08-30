ed0ebbb32421566d18dba73a9d72469766434a0a
Jason Dekarske <dekarskej@gmail.com>
2021-08-30 05:14:24 +0000

    slam is working

--- src/cablebee/launch/cameratest.launch.py
+++ src/cablebee/launch/cameratest.launch.py
@@ -7,12 +7,12 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    usb_cam_pkg = FindPackageShare('cablebee').find('cablebee')
-    webcam_cal_path = os.path.join(usb_cam_pkg, 'webcamcal', 'ost.yaml')
-    widecam_cal_path = os.path.join(usb_cam_pkg, 'widecamcal', 'ost.yaml')
+    cablebee_pkg = FindPackageShare('cablebee').find('cablebee')
+    webcam_cal_path = os.path.join(cablebee_pkg, 'webcamcal', 'ost.yaml')
+    widecam_cal_path = os.path.join(cablebee_pkg, 'widecamcal', 'ost.yaml')
+    rviz_config_file = os.path.join(cablebee_pkg, 'launch', 'cameratest.rviz')
 
     ld = LaunchDescription([
-        # declare_rviz_config_file_cmd,
         Node(package='usb_cam',
             executable='usb_cam_node_exe',
             output='screen',
@@ -29,13 +29,38 @@ def generate_launch_description():
             package='tf2_ros',
             executable='static_transform_publisher',
             output='screen',
-            arguments=['0', '0', '0', '0.0', '0.0', '0.0', 'base_link', 'camera'])
-        # Node(
-        #     package='rviz2',
-        #     executable='rviz2',
-        #     arguments=['-d', rviz_config_file],
-        #     output='screen'
-        # )
+            arguments=['0', '0', '0', '0.0', '0.0', '0.0', 'base_link', 'camera']),
+
+        DeclareLaunchArgument('transport', default_value='raw'),
+        DeclareLaunchArgument('fiducial_len', default_value='0.198'),
+        DeclareLaunchArgument('dictionary', default_value='0'),
+        DeclareLaunchArgument('do_pose_estimation', default_value='True'),
+        DeclareLaunchArgument('vis_msgs', default_value="False"),
+        DeclareLaunchArgument('ignore_fiducials', default_value=""),
+        DeclareLaunchArgument('fiducial_len_override', default_value=""),
+        Node(
+            package='aruco_detect',
+            namespace='aruco_detect',
+            executable='aruco_detect',
+            name='aruco_detect',
+            parameters=[
+                {"image_transport":LaunchConfiguration('transport')},
+                {"publish_images":True},
+                {"fiducial_len":LaunchConfiguration('fiducial_len')},
+                {"dictionary":LaunchConfiguration('dictionary')},
+                {"do_pose_estimation":LaunchConfiguration('do_pose_estimation')},
+                {"vis_msgs":LaunchConfiguration('vis_msgs')},
+                {"ignore_fiducials":LaunchConfiguration('ignore_fiducials')},
+                {"fiducial_len_override":LaunchConfiguration('fiducial_len_override')}
+            ],
+        ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            arguments=['-d', rviz_config_file],
+            output='screen'
+        )
         ])
 
     # bee_rviz = os.path.join(bee_pkg, 'launch', 'default.rviz')
