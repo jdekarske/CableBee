ea12fbd1eebb3e181e1c7e99927c29ed047d9179
Jason Dekarske <dekarskej@gmail.com>
2021-12-06 04:34:31 +0000

    add room

--- cablebee/launch/fake_float.launch.py
+++ cablebee/launch/fake_float.launch.py
@@ -8,11 +8,18 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     bee_pkg = FindPackageShare('cablebee').find('cablebee')
-    bee_urdf = os.path.join(bee_pkg, 'urdf', 'astrobee.urdf')
+    bee_desc = FindPackageShare('cablebee_description').find('cablebee_description')
+    bee_urdf = os.path.join(bee_desc, 'urdf', 'astrobee.urdf')
+    room_urdf = os.path.join(bee_desc, 'urdf', 'room.urdf')
     bee_rviz = os.path.join(bee_pkg, 'launch', 'default.rviz')
+
     with open(bee_urdf, 'r') as infp:
         robot_desc = infp.read()
-    rsp_params = {'robot_description': robot_desc}
+    rsp_cablebee_params = {'robot_description': robot_desc}
+    with open(room_urdf, 'r') as infp:
+        robot_desc = infp.read()
+    rsp_room_params = {'robot_description': robot_desc}
+
 
     rviz_config_file = LaunchConfiguration('rviz_config')
     declare_rviz_config_file_cmd = DeclareLaunchArgument(
@@ -22,8 +29,10 @@ def generate_launch_description():
     
     ld = LaunchDescription([
         declare_rviz_config_file_cmd,
+        # Node(package='robot_state_publisher', executable='robot_state_publisher',
+        #      output='screen', parameters=[rsp_cablebee_params]),
         Node(package='robot_state_publisher', executable='robot_state_publisher',
-             output='screen', parameters=[rsp_params]),
+             output='screen', parameters=[rsp_room_params]),
         Node(
             package='rviz2',
             executable='rviz2',
