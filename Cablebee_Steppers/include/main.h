237d8b9c3e3a63025e12dd50cedf494ebe475bae
Jason Dekarske <dekarskej@gmail.com>
2021-12-12 15:52:52 -0800

    spinning with interrupts

--- Cablebee_Steppers/include/main.h
+++ Cablebee_Steppers/include/main.h
@@ -4,5 +4,6 @@
 #include "PINS_BTTSKRMINIE3V2.h"
 #include "stm32f1xx_hal_rcc.h"
 #include "stm32f1xx_hal_rcc_ex.h"
+#include "stm32f1xx_hal_tim.h"
 
 #endif // MAIN_H
--- Cablebee_Steppers/src/main.c
+++ Cablebee_Steppers/src/main.c
@@ -1,41 +1,44 @@
 #include "main.h"
 
-int main(void)
+static TIM_HandleTypeDef htim2 = {
+    .Instance = TIM2};
+uint16_t step;
+uint16_t stepduration;
+uint16_t stepperiod;
+
+int error = 0;
+
+void TIM2_IRQHandler(void)
 {
-  HAL_Init();
+  HAL_TIM_IRQHandler(&htim2);
+}
 
-  // set up pins
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-
-  GPIO_InitTypeDef GPIO_InitStruct;
-  GPIO_InitStruct.Pin = X_ENABLE_PIN | X_DIR_PIN | X_STEP_PIN;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
-  HAL_GPIO_Init(X_ENABLE_PORT, &GPIO_InitStruct);
-
-  HAL_GPIO_WritePin(X_ENABLE_PORT, X_ENABLE_PIN, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(X_DIR_PORT, X_DIR_PIN, GPIO_PIN_SET);
-
-  while (1)
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  if (step == stepperiod)
   {
     HAL_GPIO_WritePin(X_STEP_PORT, X_STEP_PIN, GPIO_PIN_SET);
-    HAL_Delay(1);
-    HAL_GPIO_WritePin(X_STEP_PORT, X_STEP_PIN, GPIO_PIN_RESET);
-    HAL_Delay(10);
   }
+  else if (step > stepperiod + stepduration)
+  {
+    HAL_GPIO_WritePin(X_STEP_PORT, X_STEP_PIN, GPIO_PIN_RESET);
+    step = 0;
+  }
+
+  step++;
 }
 
 void SysTick_Handler(void)
 {
   HAL_IncTick();
+  HAL_SYSTICK_IRQHandler();
 }
-
 void SystemClock_Config(void)
 {
   RCC_ClkInitTypeDef clkinitstruct = {0};
   RCC_OscInitTypeDef oscinitstruct = {0};
+
+  // __HAL_RCC_PWR_CLK_ENABLE();
   /* Configure PLLs------------------------------------------------------*/
 
   /* Enable HSE Oscillator and activate PLL with HSE as source */
@@ -63,5 +66,65 @@ void SystemClock_Config(void)
   { /* Initialization Error */
     while (1)
       ;
+  }
+}
+
+void TIM2_Init(void)
+{
+  htim2.Init.Prescaler = 7 - 1;
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 10 - 1;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.RepetitionCounter = 0;
+  // htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    error = 1;
+  }
+}
+
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base) {
+    __HAL_RCC_TIM2_CLK_ENABLE();
+    HAL_NVIC_SetPriority(TIM2_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+void GPIO_Init()
+{
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+  GPIO_InitStruct.Pin = X_ENABLE_PIN | X_DIR_PIN | X_STEP_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+  HAL_GPIO_Init(X_ENABLE_PORT, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(X_ENABLE_PORT, X_ENABLE_PIN, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(X_DIR_PORT, X_DIR_PIN, GPIO_PIN_RESET);
+}
+
+uint16_t cnt;
+
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+  GPIO_Init();
+  TIM2_Init();
+
+  step = 0;
+  stepduration = 2; // 500 ns
+  stepperiod = 50;  // 1ms
+
+  if (HAL_TIM_Base_Start_IT(&htim2) != HAL_OK)
+  {
+    error = 1;
+  }
+
+  while (1)
+  {
   }
 }
